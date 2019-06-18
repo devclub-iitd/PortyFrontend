2753ac62a3153026c82064295805e9ee9350fec5
Shashwat Shivam <shivamshaswat@gmail.com>
2019-06-18 18:29:51 +0530

    Minor bug fix

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import '../style/portfolio.css';
 
-const Portfolio = (
+const Portfolio = () => (
   <div>
     <Paper className="portfolio-container" elevation={3} />
   </div>
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import HeaderLogin from '../components/headerLogin';
 
-const Landing = (
-  <Router>
+const Landing = () => (
     <div>
       <HeaderLogin />
     </div>
-  </Router>
 );
 
 export default Landing;
