75f1dbd7223034cd04488f7177da3d8c94ae1c00
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2019-12-16 18:33:16 +0530

    added notFound page and ui updates to reg validation

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -5,21 +5,24 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-import '../style/validation.css';
+import '../style/notFound.css';
 
 const useStyles = makeStyles(theme => ({
   button: {
-    width: '150px',
-    height: '40px',
-    marginTop: '30px',
-    borderRadius: '5px',
+    margin: theme.spacing(1),
+    width: '200px',
+    height: '50px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '40px',
+    borderRadius: '25px',
   },
   input: {
     display: 'none',
   },
 }));
 
-const Validation = () => {
+const NotFound = () => {
   const classes = useStyles();
   return (
     <div>
@@ -28,32 +31,28 @@ const Validation = () => {
           <Typography>
             <span style={{ fontWeight: 700, fontSize: '20px' }}>Portfolio Creator</span>
             {' '}
-            <span style={{ color: '#3d40d8' }}>| Verification</span>
+            <span style={{ color: '#3d40d8' }}>| Error 404</span>
           </Typography>
         </Toolbar>
       </AppBar>
-      <div className="fullScreen" style={{ marginLeft: '60px' }}>
+      <div className="fullScreen">
         <div className="notFoundTextContainer">
           <Typography variant="h2">
-            : )
+            : (
           </Typography>
-          <Typography variant="h3" style={{ marginTop: '80px', fontWeight: '600' }}>
-            Verification Successful :)
+          <Typography variant="h3" style={{ marginTop: '50px' }}>
+            Whoops!!
           </Typography>
-          <Typography style={{ marginTop: '30px', fontSize: '18px', fontWeight: '300' }}>
-            Your email has been verified successfully.
-            {' '}
-            Kindly click the button below to proceed to the login page
-            {' '}
-            <b>Login with your newly created account.</b>
+          <Typography variant="h5" style={{ marginTop: '20px' }}>
+            The page you are looking for is not available.
           </Typography>
         </div>
         <Button variant="contained" color="secondary" className={classes.button}>
-          Login
+          Home
         </Button>
       </div>
     </div>
   );
 };
 
-export default Validation;
+export default NotFound;
