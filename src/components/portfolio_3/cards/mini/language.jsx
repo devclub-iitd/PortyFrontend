4afa30ad5202f180a34d2d3205ab9d07887ce2a3
Shashwat Shivam <shivamshaswat@gmail.com>
2020-05-12 16:09:03 +0530

    Lint fixes

--- src/components/portfolio_3/cards/mini/language.jsx
+++ src/components/portfolio_3/cards/mini/language.jsx
@@ -6,32 +6,36 @@ import Chip from '@material-ui/core/Chip';
 import Typography from '@material-ui/core/Typography';
 
 const MiniCard = (props) => {
-  const {
-    title, items,
-  } = props;
+    const { title, items } = props;
 
-  return (
-    <Card className="portfolioExtraCard">
-      <div className="portfolioExtraDetails">
-        <CardContent className="portfolioExtraContent">
-          <Typography component="h5" variant="h5" style={{ textTransform: 'capitalize' }}>
-            { title }
-          </Typography>
-          {
-            items.map(
-              item => <Chip className="chip" variant="outlined" label={item.language} />,
-            )
-          }
-        </CardContent>
-      </div>
-      <div className="portfolioExtraImage miniLanguageImage" />
-    </Card>
-  );
+    return (
+        <Card className="portfolioExtraCard">
+            <div className="portfolioExtraDetails">
+                <CardContent className="portfolioExtraContent">
+                    <Typography
+                        component="h5"
+                        variant="h5"
+                        style={{ textTransform: 'capitalize' }}
+                    >
+                        {title}
+                    </Typography>
+                    {items.map((item) => (
+                        <Chip
+                            className="chip"
+                            variant="outlined"
+                            label={item.language}
+                        />
+                    ))}
+                </CardContent>
+            </div>
+            <div className="portfolioExtraImage miniLanguageImage" />
+        </Card>
+    );
 };
 
 MiniCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  items: PropTypes.arrayOf.isRequired,
+    title: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf.isRequired,
 };
 
 export default MiniCard;
