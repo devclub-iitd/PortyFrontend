998a5e96e8a077221b5136df183358c94755af9d
Japneet Singh <singhjapneetssa@gmail.com>
2020-06-11 14:05:39 +0530

    Added Neu Light as Template 6

---
+++ Designs/PortyDisplayDesign6/src/portfolio/cards/expand.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
+
+const ExpansionCard = (props) => {
+    const {
+        organisation,
+        position,
+        children,
+        website,
+        startDate,
+        endDate,
+        handlePanelChange,
+        expanded,
+        id,
+    } = props;
+
+    return (
+        <ExpansionPanel
+            expanded={expanded === `panel${id}`}
+            onChange={() => handlePanelChange(`panel${id}`)}
+            className="portfolioVolunteerExpansionPanel"
+        >
+            <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon className="expandIcon"/>}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+                className="expansionSummary"
+            >
+                <div className="portfolioExpansionPanelTitle">
+                    {organisation}
+                </div>
+                <Typography className="portfolioExpansionPanelSubTitle">
+                    {position}
+                </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails className="expansionExpanded">
+                <div className="portfolioExpansionPanelDetails">
+                    <Typography>{children}</Typography>
+                    <div className="miniLine miniCardLine" />
+                    <div className="portfolioExpansionPanelWebsite">
+                        <span>Website |</span>{' '}
+                        <a
+                            href={website}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {website}
+                        </a>
+                    </div>
+                    <div className="portfolioCardDateContainer">
+                        {startDate} | {endDate}
+                    </div>
+                </div>
+            </ExpansionPanelDetails>
+        </ExpansionPanel>
+    );
+};
+
+ExpansionCard.propTypes = {
+    organisation: PropTypes.string.isRequired,
+    position: PropTypes.string.isRequired,
+    children: PropTypes.string.isRequired,
+    website: PropTypes.string.isRequired,
+    startDate: PropTypes.string.isRequired,
+    endDate: PropTypes.string.isRequired,
+    handlePanelChange: PropTypes.func.isRequired,
+    expanded: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+};
+
+export default ExpansionCard;
---
+++ Designs/PortyDisplayDesign6/src/portfolio/work.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import PaperCard from './cards/paper';
+
+const Work = (props) => {
+    const { work } = props;
+
+    return (
+        <div className="portfolioPage workPage">
+            <div className="portfolioPageTitle">
+            <div className="titleCard">Work</div></div>
+            <div className="portfolioCardContainer portfolioWorkCardContainer">
+                {work.map((workPlace) => (
+                    <PaperCard
+                        company={workPlace.company}
+                        position={workPlace.position}
+                        website={workPlace.website}
+                        startDate={workPlace.startdate}
+                        endDate={workPlace.enddate}
+                        className='workCard'
+                    >
+                        {workPlace.summary}
+                    </PaperCard>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+Work.propTypes = {
+    work: PropTypes.objectOf(PropTypes.object).isRequired,
+};
+
+export default Work;
