948d304ab260ff722192c7b9bc509b648870803b
Japneet Singh <singhjapneetssa@gmail.com>
2020-07-02 23:03:01 +0530

    more fixes

--- Designs/PortyDisplayDesign5/src/App.jsx
+++ Designs/PortyDisplayDesign5/src/App.jsx
@@ -646,7 +646,7 @@ function App(props) {
                 </Parallax>
             )}
             {windowSize.height <= 500 && (
-                <Parallax pages={7}>
+                <Parallax pages={8}>
                     <ParallaxLayer
                         offset={0}
                         speed={0}
@@ -700,6 +700,262 @@ function App(props) {
                         style={{
                             backgroundColor: '#000000',
                             opacity: 0.8,
+                            
+                        }}
+                    />
+                    <ParallaxLayer
+                        offset={8}
+                        speed={0}
+                        style={{
+                            backgroundColor: '#000000',
+                            opacity: 1,
+                            
+                        }}
+                    />
+                    <ParallaxLayer offset={0} speed={0}>
+                        <HeaderBackground />
+                    </ParallaxLayer>
+                    <ParallaxLayer offset={0} speed={0.3}>
+                        <HeaderIcons />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                        offset={0}
+                        speed={0.8}
+                        style={{ opacity: 0.4 }}
+                    >
+                        <Cloud
+                            style={{
+                                display: 'block',
+                                width: '20%',
+                                marginLeft: '25%',
+                            }}
+                        />
+                        <Cloud
+                            style={{
+                                display: 'block',
+                                width: '10%',
+                                marginLeft: '45%',
+                            }}
+                        />
+                    </ParallaxLayer>
+                    <ParallaxLayer
+                        offset={1}
+                        speed={-0.2}
+                        style={{ opacity: 0.7,  }}
+                    >
+                        <Cloud
+                            style={{
+                                display: 'block',
+                                width: '20%',
+                                marginLeft: '55%',
+                            }}
+                        />
+                        <Cloud
+                            style={{
+                                display: 'block',
+                                width: '10%',
+                                marginLeft: '15%',
+                            }}
+                        />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                        offset={0.25}
+                        speed={0.3}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <DataAnalytics
+                            style={{
+                                width: `${
+                                    window.innerWidth < 960 ? '40%' : '20%'
+                                }`,
+                                marginLeft: '60%',
+                            }}
+                        />
+                    </ParallaxLayer>
+                    <ParallaxLayer
+                        offset={1.15}
+                        speed={0.7}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Sun style={{ width: '20%', marginLeft: '20%' }} />
+                    </ParallaxLayer>
+                    <ParallaxLayer
+                        offset={2.25}
+                        speed={0.9}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <DataProcess
+                            style={{
+                                width: '70%',
+                                marginRight: '10%',
+                                opacity: '0.7',
+                            }}
+                        />
+                    </ParallaxLayer>
+                    <ParallaxLayer
+                        offset={3.25}
+                        speed={0.9}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <ProcessBuilding
+                            style={{ width: '20%', marginRight: '60%' }}
+                        />
+                    </ParallaxLayer>
+                    <ParallaxLayer
+                        offset={3}
+                        speed={-0.2}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Mountain
+                            style={{ width: '20%', marginLeft: '60%' }}
+                        />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                        offset={-0.4}
+                        speed={-0.2}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            marginLeft: '10%',
+                            cursor: 'pointer',
+                        }}
+                    >
+                        <Landing
+                            name={profile.user.name}
+                            label={profile.about.label}
+                            img={profile.about.imgUrl}
+                            initScroll={scrollFunc}
+                        />
+                    </ParallaxLayer>
+                    <ParallaxLayer offset={1} speed={0.3} />
+                    <ParallaxLayer offset={1} speed={0.3}>
+                        <About
+                            summary={profile.about}
+                            top={window.innerHeight}
+                        />
+                        <Education education={profile.education} />
+                        <Work work={profile.work} />
+                        <Volunteer volunteer={profile.volunteer} />
+                        <Extra
+                            awards={profile.awards}
+                            publications={profile.publications}
+                            languages={profile.languages}
+                            skills={profile.skills}
+                        />
+                        <Contact
+                            email={profile.user.email}
+                            phone={profile.about.number}
+                            location={profile.location}
+                        />
+                    </ParallaxLayer>
+                    <ParallaxLayer offset={4.5} speed={0.3} />
+                </Parallax>
+            )}
+            {windowSize.height <= 300 && (
+                <Parallax pages={10}>
+                    <ParallaxLayer
+                        offset={0}
+                        speed={0}
+                        style={{ backgroundColor: '#00745F',  }}
+                    />
+                    <ParallaxLayer
+                        offset={1}
+                        speed={0}
+                        style={{ backgroundColor: '#0A1034',  }}
+                    />
+                    <ParallaxLayer
+                        offset={2}
+                        speed={0}
+                        style={{ backgroundColor: '#65B0B1',  }}
+                    />
+                    <ParallaxLayer
+                        offset={3}
+                        speed={0}
+                        style={{ backgroundColor: '#424C5E',  }}
+                    />
+                    <ParallaxLayer
+                        offset={4}
+                        speed={0}
+                        style={{
+                            backgroundColor: 'rgb(1, 0, 34)',
+                            opacity: 0.8,
+                            
+                        }}
+                    />
+                    <ParallaxLayer
+                        offset={5}
+                        speed={0}
+                        style={{
+                            backgroundColor: 'rgb(1, 0, 34)',
+                            opacity: 0.8,
+                            
+                        }}
+                    />
+                    <ParallaxLayer
+                        offset={6}
+                        speed={0}
+                        style={{
+                            backgroundColor: '#140951',
+                            opacity: 0.8,
+                            
+                        }}
+                    />
+                    <ParallaxLayer
+                        offset={7}
+                        speed={0}
+                        style={{
+                            backgroundColor: '#000000',
+                            opacity: 0.8,
+                            
+                        }}
+                    />
+                    <ParallaxLayer
+                        offset={8}
+                        speed={0}
+                        style={{
+                            backgroundColor: '#000000',
+                            opacity: 0.9,
+                            
+                        }}
+                    />
+                    <ParallaxLayer
+                        offset={9}
+                        speed={0}
+                        style={{
+                            backgroundColor: '#000000',
+                            opacity: 0.1,
+                            
+                        }}
+                    />
+                    <ParallaxLayer
+                        offset={10}
+                        speed={0}
+                        style={{
+                            backgroundColor: '#000000',
+                            opacity: 1,
                             
                         }}
                     />
